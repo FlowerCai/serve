f583567964a97bd7dd6765e7ce14477baa4ce0c1
caishifang <fangcsyuan79@163.com>
2022-10-01 23:06:56 +0800

    add index.html

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 let express = require("express");
 const api = require("./api");
 let router = express.Router();
+let path = require('path')
 /**
  * code : 0 -> sql查询失败;msg -> 错误提示
  *        1 -> 查询成功;data -> 返回数据
@@ -13,4 +14,9 @@ router.get("/getscorelist", (req, res, next) => {
 router.post("/addscore", (req, res, next) => {
   api.addScore(req, res, next);
 });
+
+router.get("/", (req, res, next)=> {
+  res.sendFile(path.join(__dirname, "index.html"));
+})
+
 module.exports = router;
